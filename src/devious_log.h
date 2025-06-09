f2b312ee428132944a2b8b7e8d18681a5280bc4d
Bartosz Moczulski <moczulski@gmail.com>
2025-06-09 13:10:26 +0200

    chore: Add missing <stdio.h> in devious_log.h
    The file defines a macro which uses printf(), it should obviously include
    its dependency.

--- src/devious_log.h
+++ src/devious_log.h
@@ -11,4 +11,6 @@
 #define YELLOW  "\033[33m"      /* Yellow */
 #define RED     "\033[31m"      /* Red */
 #define RESET   "\033[0m"
+
+#include <stdio.h>
 #define devious_log(f, ...) fprintf(stderr, "\n" RED "]:-> " YELLOW f RESET "\n", ##__VA_ARGS__ )
